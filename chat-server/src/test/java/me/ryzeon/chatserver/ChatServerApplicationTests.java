7cc52d1e11428b36a20771bc19836d4a19d98370
Ryzeon <alex.ramon2004@gmail.com>
2024-05-15 17:09:03 -0500

    feat: init server module.

---
+++ chat-server/src/test/java/me/ryzeon/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ryzeon.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
