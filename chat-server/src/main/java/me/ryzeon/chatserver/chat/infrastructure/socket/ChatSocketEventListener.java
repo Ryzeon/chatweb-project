7cdf32048b038ec6608227f666dddeda5589fb3c
Ryzeon <alex.ramon2004@gmail.com>
2024-05-15 20:26:05 -0500

    feat: added mongodb dependency.

---
+++ chat-server/src/main/java/me/ryzeon/chatserver/chat/infrastructure/socket/ChatSocketEventListener.java
@@ -0,0 +1,9 @@
+package me.ryzeon.chatserver.chat.infrastructure.socket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ChatSocketEventListener {
+}
